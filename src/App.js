636f2098324b0eaf43139701921d7e016e1cf0f9
wael senani <waelsenani106@gmail.com>
2023-11-09 18:58:47 +0100

    timeline container v1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Footer from "./components/footer";
 import Header from "./components/header";
+import TimeLine from "./components/timeLine";
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <TimeLine/>
+      <Footer/>
       <p style={{fontFamily:"'Noto Nastaliq Urdu', serif"}}>عدد كبير من الشهداء.. القصف الإسرائيلي يستهدف منزلين في مخيم جباليا</p>
       <p style={{fontFamily:"'Cairo', sans-serif"}}>عدد كبير من الشهداء.. القصف الإسرائيلي يستهدف منزلين في مخيم جباليا</p>
       <p style={{fontFamily:"'Reem Kufi Fun', sans-serif"}}>عدد كبير من الشهداء.. القصف الإسرائيلي يستهدف منزلين في مخيم جباليا</p>
-      <Footer/>
     </div>
   );
 }
---
+++ src/components/timeLine.js
@@ -0,0 +1,93 @@
+
+const TimeLine = () => {
+  return (
+    <div id="timeLineContainer">
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+        <div className="eventContainer">
+            <div className="event">
+                <h1>title</h1>
+                <p>description</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TimeLine
